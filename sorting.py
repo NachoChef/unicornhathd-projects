d7b524129f55633766aa9fe1f7bc4318268dfcb8
Justin Jones <hock3ydud3@gmail.com>
2018-10-11 18:15:15 -0600

    converted rgb values to tuple, moved sum to [-1]

--- sorting.py
+++ sorting.py
@@ -12,10 +12,8 @@ def build_array():
     for _ in range(disp_x):
         row_sum = random.randint(1, disp_y)
         row = [1 if ind < row_sum else 0 for ind in range(disp_y)]
+        row.append((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))
         row.append(row_sum)
-        row.append(random.randint(1, 255))
-        row.append(random.randint(1, 255))
-        row.append(random.randint(1, 255))
         disp.append(row)
     return disp
 #end build_array
@@ -24,14 +22,14 @@ def show_array(arr):
     for x in range(len(arr)):
         for y in range(len(arr)):
             val = arr[x][y]
-            uh.set_pixel(x, y, arr[x][y] * arr[x][-1], arr[x][y] * arr[x][-2], arr[x][y] * arr[x][-3])
+            uh.set_pixel(x, y, arr[x][y] * arr[x][-2][0], arr[x][y] * arr[x][-2][1], arr[x][y] * arr[x][-2][2])
     uh.show()
 #end show_array
 
 def bubble_sort(arr):
     for i in range(len(arr)):
         for j in range(len(arr) - 1 - i):
-            if arr[j][-4] > arr[j+1][-4]:
+            if arr[j][-1] > arr[j+1][-1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 show_array(arr)
     return arr
@@ -41,11 +39,11 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         current = arr[i]
         pos = i
-        while pos > 0 and arr[pos-1][-4] > current[-4]:
+        while pos > 0 and arr[pos-1][-1] > current[-1]:
             arr[pos] = arr[pos-1]
             pos = pos - 1
             show_array(arr)
-            time.sleep(0.1)
+            #time.sleep(0.1)
           
         arr[pos] = current
         show_array(arr)
@@ -53,7 +51,7 @@ def insertion_sort(arr):
 #end insertion_sort
 
 def quicksort(arr):
-    # array is [16 * (0 or 1), sum, r, g, b]
+    # array is [16 * (0 or 1), (r, g, b), sum]
     
     # partitions
     less, equal, greater = [], [], []
@@ -61,14 +59,14 @@ def quicksort(arr):
     
     # this checks that there is more than 1 col in the list
     if len(arr) > 1 and type(arr[0]) is list:
-        # pivot is a 20digit col with size at -4
+        # pivot is a 20digit col with size at -1
         pivot = arr[len(arr) // 2]
         for col in arr:
-            if col[-4] < pivot[-4]:
+            if col[-1] < pivot[-1]:
                 less.append(col)
-            if col[-4] == pivot[-4]:
+            if col[-1] == pivot[-1]:
                 equal.append(col)
-            if col[-4] > pivot[-4]:
+            if col[-1] > pivot[-1]:
                 greater.append(col)
             show_array(less+equal+greater)
         return quicksort(less) + equal + quicksort(greater) 
