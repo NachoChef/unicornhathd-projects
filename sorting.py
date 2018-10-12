e5d3c3a1766dddd1d52f47adcdf1b473c187d7e0
Justin Jones <hock3ydud3@gmail.com>
2018-10-11 20:49:09 -0500

    more efficient bubbling

--- sorting.py
+++ sorting.py
@@ -62,12 +62,25 @@ def display(arr):
 
 def bubble_sort(arr):
     for i in range(len(arr)):
+        swaps = 0
         for j in range(len(arr) - 1 - i):
             if arr[j][SUM_IND] > arr[j+1][SUM_IND]:
+                swaps = swaps + 1
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 arr[j+1][MOD_IND], arr[j][MOD_IND] = True, True
+                
+                # "bubble the hole"
+                k = j
+                while k > 0:
+                    if arr[k][SUM_IND] < arr[k-1][SUM_IND]:
+                        arr[k][SUM_IND], arr[k-1][SUM_IND] = arr[k-1][SUM_IND], arr[k][SUM_IND]
+                        arr[k][MOD_IND], arr[k-1][MOD_IND] = True, True
+                        k = k - 1
+                        
                 show_array(arr)
                 pause()
+        if swaps is 0:
+            break
     return arr
 # end bubble_sort
 
