00cb1e2300a8235713c05ee2533384385c11e9e2
Justin Jones <hock3ydud3@gmail.com>
2018-10-11 19:19:59 -0600

    Insertion sort printing is slightly borked

--- sorting.py
+++ sorting.py
@@ -1,15 +1,33 @@
 import unicornhathd as uh
 import random, time
+from utils import greeting
 
+# rotate so that HDMI port is pointing up
 uh.rotation(270)
+
+# half brightness because camera :)
 uh.brightness(0.5)
+
+# RGB dimensions in case...something
 DISP_X, DISP_Y = uh.get_shape()
 
+# the sum of the column stored here:
 SUM_IND = -1
+
+# the random color tuple for the column stored here:
 RGB_IND = -2
+
+# the status of the column stored here:
 MOD_IND = -3
 
+# whether or not to activate slow mode
+slow_mode = True
+
+def pause():
+    time.sleep(0.1) if slow_mode else None
+
 def build_array():
+    r''' Generates random columns with appropriate data '''
     disp = []
     for _ in range(DISP_X):
         row_sum = random.randint(1, DISP_Y)
@@ -19,17 +37,28 @@ def build_array():
         row.append(row_sum)
         disp.append(row)
     return disp
-#end build_array
+# end build_array
 
 def show_array(arr, efficient=True):
+    r'''  Will loop through and display array. If `efficient` then only modified rows will be written to the buffer. '''
     for x in range(len(arr)):
-        if arr[x][MOD_IND] is True or not efficient:    # if efficient mode activated, only update buffer if row is changed!
+        if arr[x][MOD_IND] is True or not efficient: 
             arr[x][MOD_IND] = False
             for y in range(len(arr)):
-                val = arr[x][y]
-                uh.set_pixel(x, y, arr[x][y] * arr[x][RGB_IND][0], arr[x][y] * arr[x][RGB_IND][1], arr[x][y] * arr[x][RGB_IND][2])
+                state = arr[x][y]
+                rgb = arr[x][RGB_IND]
+                # if position value is 1 then we want to add color info, otherwise clear color info so we don't draw anything
+                uh.set_pixel(x, y, state * rgb[0], state * rgb[1], state * rgb[2])
     uh.show()
-#end show_array
+# end show_array
+    
+def display(arr):
+    for x in range(len(arr)):
+        for y in range(len(arr)):
+            pixel = arr[x][y]
+            uh.set_pixel(x, y, pixel * 100, pixel * 100, pixel * 100)
+    uh.show()
+# end display
 
 def bubble_sort(arr):
     for i in range(len(arr)):
@@ -38,26 +67,26 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 arr[j+1][MOD_IND], arr[j][MOD_IND] = True, True
                 show_array(arr)
+                pause()
     return arr
-#end bubble_sort
+# end bubble_sort
 
 def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        current = arr[i]
-        pos = i
+    for pos in range(1, len(arr)):
+        current = arr[pos]
+        current[MOD_IND] = True
         while pos > 0 and arr[pos-1][SUM_IND] > current[SUM_IND]:
             arr[pos] = arr[pos-1]
-            arr[pos][MOD_IND], arr[pos][MOD_IND] = True, True
+            arr[pos][MOD_IND], arr[pos-1][MOD_IND] = True, True
             pos = pos - 1
             show_array(arr)
-            #time.sleep(0.1)
+            pause()
           
         arr[pos] = current
-        arr[pos][MOD_IND] = True
         show_array(arr)
     show_array(arr, False)
     return arr
-#end insertion_sort
+# end insertion_sort
 
 def quicksort(arr):
     # array is [16 * (0 or 1), (r, g, b), sum]
@@ -68,35 +97,44 @@ def quicksort(arr):
     
     # this checks that there is more than 1 col in the list
     if len(arr) > 1 and type(arr[0]) is list:
-        # pivot is a 20digit col with size at SUM_IND
+        # pivot is a 19 item list with size at SUM_IND
         pivot = arr[len(arr) // 2]
         for col in arr:
             if col[SUM_IND] < pivot[SUM_IND]:
                 less.append(col)
-            if col[SUM_IND] == pivot[SUM_IND]:
+            if col[SUM_IND] is pivot[SUM_IND]:
                 equal.append(col)
             if col[SUM_IND] > pivot[SUM_IND]:
                 greater.append(col)
-            show_array(less+equal+greater, False)
+            show_array(less + equal + greater, False)
         return quicksort(less) + equal + quicksort(greater) 
     else: 
         return arr
-#end quicksort
+# end quicksort
 
 def main():
-    my_array = build_array()
-    show_array(my_array, False)
+    uh.rotation(180)
+    display(greeting)
     time.sleep(1.0)
-    arr = insertion_sort(my_array)
+    unsorted_array = build_array()
+    uh.rotation(270)
+    show_array(unsorted_array, False)
+    time.sleep(1.0)
+    arr = bubble_sort(list(unsorted_array))
+    #show_array(arr, False)
+    time.sleep(1.0)
+    show_array(unsorted_array, False)
+    time.sleep(1.0)
+    arr = insertion_sort(list(unsorted_array))
+    #show_array(arr, False)
+    time.sleep(1.0)
+    show_array(unsorted_array, False)
+    time.sleep(1.0)
+    arr = quicksort(list(unsorted_array))
     show_array(arr, False)
-    #time.sleep(1.0)
-    #show_array(my_array)
-    #time.sleep(1.0)
-    #arr = quicksort(my_array)
-    #show_array(arr)
     time.sleep(1)
     uh.off()
-#end main
+# end main
 
 if __name__ == '__main__':
     main()
