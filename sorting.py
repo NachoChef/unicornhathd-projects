6fe30f40d3b7c3d6813d30a7e426e543fa10179c
Justin Jones <hock3ydud3@gmail.com>
2018-10-12 23:30:10 -0500

    local library modification tests

--- sorting.py
+++ sorting.py
@@ -23,24 +23,38 @@ MOD_IND = -3
 # whether or not to activate slow mode
 slow_mode = True
 
+
 def pause():
     time.sleep(0.1) if slow_mode else None
 
+
 def build_array():
-    r''' Generates random columns with appropriate data '''
-    disp = []
-    for _ in range(DISP_X):
+    """Generates random columns with appropriate data."""
+    disp = uh.get_pixels()
+    for i in range(len(disp)):
         row_sum = random.randint(1, DISP_Y)
-        row = [1 if ind < row_sum else 0 for ind in range(DISP_Y)]
-        row.append(False)
-        row.append((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))
-        row.append(row_sum)
-        disp.append(row)
+        disp[i] = [[random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)]
+                   if ind < row_sum else [[0] * 3] for ind in range(DISP_Y)]
+        disp[i].append(row_sum)
+
+    # for _ in range(DISP_X):
+    #     row_sum = random.randint(1, DISP_Y)
+    #     row = [1 if ind < row_sum else 0 for ind in range(DISP_Y)]
+    #     row.append(False)
+    #     row.append((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))
+    #     row.append(row_sum)
+    #     disp.append(row)
     return disp
 # end build_array
 
-def show_array(arr, efficient=True):
-    r'''  Will loop through and display array. If `efficient` then only modified rows will be written to the buffer. '''
+
+def update_buffer(arr, efficient=True):
+    """Will loop through and display array. If `efficient` then only modified rows will be written to the buffer.
+
+    :param arr: new buffer array
+    :param efficient:
+    :return:
+    """
     for x in range(len(arr)):
         if arr[x][MOD_IND] is True or not efficient: 
             arr[x][MOD_IND] = False
@@ -51,7 +65,8 @@ def show_array(arr, efficient=True):
                 uh.set_pixel(x, y, state * rgb[0], state * rgb[1], state * rgb[2])
     uh.show()
 # end show_array
-    
+
+
 def display(arr):
     for x in range(len(arr)):
         for y in range(len(arr)):
@@ -59,6 +74,7 @@ def display(arr):
             uh.set_pixel(x, y, pixel * 100, pixel * 100, pixel * 100)
     uh.show()
 # end display
+
 
 def bubble_sort(arr):
     for i in range(len(arr)):
@@ -77,12 +93,13 @@ def bubble_sort(arr):
                         arr[k][MOD_IND], arr[k-1][MOD_IND] = True, True
                         k = k - 1
                         
-                show_array(arr)
+                update_buffer(arr)
                 pause()
         if swaps is 0:
             break
     return arr
 # end bubble_sort
+
 
 def insertion_sort(arr):
     for pos in range(1, len(arr)):
@@ -92,14 +109,15 @@ def insertion_sort(arr):
             arr[pos] = arr[pos-1]
             arr[pos][MOD_IND], arr[pos-1][MOD_IND] = True, True
             pos = pos - 1
-            show_array(arr)
+            update_buffer(arr)
             pause()
           
         arr[pos] = current
-        show_array(arr)
-    show_array(arr, False)
+        update_buffer(arr)
+    update_buffer(arr, False)
     return arr
 # end insertion_sort
+
 
 def quicksort(arr):
     # array is [16 * (0 or 1), (r, g, b), sum]
@@ -119,11 +137,12 @@ def quicksort(arr):
                 equal.append(col)
             if col[SUM_IND] > pivot[SUM_IND]:
                 greater.append(col)
-            show_array(less + equal + greater, False)
+            update_buffer(less + equal + greater, False)
         return quicksort(less) + equal + quicksort(greater) 
     else: 
         return arr
 # end quicksort
+
 
 def main():
     uh.rotation(180)
@@ -131,24 +150,25 @@ def main():
     time.sleep(1.0)
     unsorted_array = build_array()
     uh.rotation(270)
-    show_array(unsorted_array, False)
+    update_buffer(unsorted_array, False)
     time.sleep(1.0)
     arr = bubble_sort(list(unsorted_array))
     #show_array(arr, False)
     time.sleep(1.0)
-    show_array(unsorted_array, False)
+    update_buffer(unsorted_array, False)
     time.sleep(1.0)
     arr = insertion_sort(list(unsorted_array))
     #show_array(arr, False)
     time.sleep(1.0)
-    show_array(unsorted_array, False)
+    update_buffer(unsorted_array, False)
     time.sleep(1.0)
     arr = quicksort(list(unsorted_array))
-    show_array(arr, False)
+    update_buffer(arr, False)
     time.sleep(1)
     uh.off()
 # end main
 
+
 if __name__ == '__main__':
     main()
-    
+
